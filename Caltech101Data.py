15edcae3d080259203688cebee1fca1e23023328
Bhashithe Abeysinghe <tabeysinghe2@student.gsu.edu>
2018-09-21 19:13:35 -0400

    Dataset file and README

---
+++ Caltech101Data.py
@@ -0,0 +1,27 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import cv2
+
+class Caltech101Data(Dataset):
+	""" Caltech101 dataset """
+
+	def __init__(self, inpath, transform=None):
+		self.transform = transform
+		self.path = inpath
+		self.image_list = os.listdir(self.path)
+
+	def __len__(self):
+		return len(self.image_list)
+
+	def __getitem__(self, idx):
+		print(self.image_list[idx])
+		image_path = os.path.join(self.path,self.image_list[idx])
+		image = cv2.imread(image_path)
+		label = self.image_list[idx].split('_image')[0]
+
+		if self.transform:
+			self.transform(image)
+		sample = {'image': image, 'label': label}
+		
+		return sample
---
+++ README.md
@@ -0,0 +1,3 @@
+# Issues
+[-] TypeError: 'builtin_function_or_method' object is not iterable
+	- Coming from transformations, check
