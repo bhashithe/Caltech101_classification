da004f726e7196c96bd3cccc8a7fbda66c63dba4
Bhashithe Abeysinghe <tabeysinghe2@student.gsu.edu>
2018-09-21 19:11:20 -0400

    Dataset class created

--- classifier.py
+++ classifier.py
@@ -1,30 +1,36 @@
 import torch
 from torch import nn
 from torch.nn import functional
-import torch.optim as optim
+
+class ConvUnit(nn.Module):
+	def __init__(self, in_channels, out_channels):
+		super(ConvUnit, self).__init__()
+		self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1)
+		self.norm = nn.BatchNorm2d(num_features=out_channels)
+		self.acti = nn.ReLU()
+	
+	def forward(self, input):
+		output = self.conv(input)
+		output = self.norm(output)
+		output = self.acti(output)
+
+		return output
 
 class Classifier(nn.Module):
-	def __init__(self):
+	def __init__(self, num_classes):
 		super(Classifier, self).__init__()
-		self.conv1 = nn.Conv2d(3,6,5)
-		self.pool = nn.MaxPool2d(2,2)
-		self.conv2 = nn.Conv2d(6,16,5)
-		self.fc1 = nn.Linear(16*5*5,120)
-		self.fc2 = nn.Linear(120,84)
-		self.fc3 = nn.Linear(84,10)
+		self.unit0 = ConvUnit(3, 32)
+		self.unit1 = ConvUnit(32, 32)
+		self.pool0 = nn.MaxPool2d(2, stride=1)
+		self.unit2 = ConvUnit(32, 64)
+		self.unit3 = ConvUnit(64, 64)
+		self.pool2 = nn.AvgPool2d(3, stride=1)
+		self.network = nn.Sequential(self.unit0, self.unit1, self.pool0, self.unit2, self.unit3, self.pool1)
 
-	def forward(self,x):
-		x = self.pool(functional.relu(self.conv1(x)))
-		x = self.pool(functional.relu(self.conv2(x)))
-		x = x.view(-1,16*5*5)
-		x = functional.relu(self.fc1(x))
-		x = functional.relu(self.fc2(x))
-		x = self.fc3(x)
-		
-		return x
+		self.fc = nn.Linear(in_features=64, num_classes=num_classes)
 
+	def forward(self, input):
+		output = self.network(input)
+		output = self.fc(output.view(-1, 128))
 
-class Trainer():
-	classifier = Classifier()
-	criterion = nn.CrossEntropyLoss()
-	optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9)
+		return output
