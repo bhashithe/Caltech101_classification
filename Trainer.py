817484a2d611b80fb3e5e24930dd81e9656de48c
Bhashithe Abeysinghe <bhashithe@gmail.com>
2018-09-27 12:29:07 -0400

    added model checkpoint saving

--- Trainer.py
+++ Trainer.py
@@ -12,7 +12,7 @@ class Trainer():
 		self.loss_function = loss_function
 		self.model = model
 
-	def train(self,epochs):
+	def train(self,epochs, loss=None):
 		for epoch in range(epochs):
 			running_loss = 0.0
 			for batch_idx, data in enumerate(self.loader,0):
@@ -31,6 +31,11 @@ class Trainer():
 				if batch_idx%2000==0:
 					print('[%d, %5d] loss: %.3f' % (epoch+1, batch_idx+1, running_loss/2000))
 					running_loss = 0.0
+
+				# save the model state in each beatch so that it is possible to resume training later
+				torch.save({'epoch': epoch, 'model_state_dict': self.model.state_dict(), 'optimizer_state_dict': self.optimizer.state_dict(), 'loss': loss}, 'model_checkpoint.mdl')
 		print('finished training')
+		# we can save the whole model
+		torch.save(self.model, 'model_complete.mdl')
 
 
--- test.py
+++ test.py
@@ -7,18 +7,25 @@ from torch.utils.data import DataLoader
 from torch.optim import Adam
 
 tr = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.RandomVerticalFlip(), transforms.Resize((224,224))])
-
+model = Classifier(102) # or you can torch.load(model_complete.mdl)
 cd = Caltech101Data('image_label', tr)
-
-model = Classifier(102)
-adam = Adam(model.parameters())
+optimizer = Adam(model.parameters())
 loss_function = torch.nn.CrossEntropyLoss()
 
+#'''
+# if you want to load a checkpoint of a model
+checkpoint = torch.load('model_checkpoint.mdl')
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+epoch = checkpoint['epoch']
+loss = checkpoint['loss']
+#'''
+
 loader = DataLoader(cd, batch_size=20, shuffle=True)
-trainer = Trainer(loader, cd, adam, loss_function, model)
+trainer = Trainer(loader, cd, optimizer, loss_function, model)
 
 def main():
-	trainer.train(2)
+	trainer.train(2, loss)
 
 if __name__ == '__main__':
 	main()
